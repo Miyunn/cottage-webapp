a1743524d24ac89a5218fb59c85a3de5642910ed
Kavinthe Perera <kavintheperera@gmail.com>
2022-12-02 22:15:01 +0530

    Update Intro.jsx

--- src/containers/intro/Intro.jsx
+++ src/containers/intro/Intro.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Feature from '../../components/feature/Feature'
-import './intro.css'
+import React from "react";
+import Feature from "../../components/feature/Feature";
+import "./intro.css";
 
 const Intro = () => {
   return (
-    <div className='intro section__margin' id='wgpt3'>
-
-      <div className='intro-heading'>
-        <h1 className='gradient__text'>A Cottage Where Modern Meets Tradition in an Ancident City</h1>
-        <a href='#gallery'>
+    <div className="intro section__margin" id="wgpt3">
+      <div className="intro-heading">
+        <h1 className="gradient__text">
+          A Cottage Where Modern Meets Tradition in an Ancident City
+        </h1>
+        <a href="#gallery">
           <p>Explore our Gallery</p>
         </a>
       </div>
-   
-    <div className='intro-container'>
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+
+      <div className="intro-container">
+        {/* <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
       <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
-    </div> 
-  </div>
-  
+      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" /> */}
+      </div>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Intro
+export default Intro;
