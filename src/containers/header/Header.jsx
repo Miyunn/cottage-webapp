8e25e693adeb4110a8100ec49b4232cbf7a5a8aa
Kavinthe Perera <kavintheperera@gmail.com>
2022-12-03 04:10:46 +0530

    AOS init

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./header.css";
 import header from "../../assets/header.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-const Header = () => (
-  <div className="header section__padding" id="home">
-    <div className="header-content">
-      <h1 className="gradient__text">KANDY API COTTAGE</h1>
+const Header = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+  return (
+    <div className="header section__padding" id="home">
+      <div className="header-content" data-aos="fade-right">
+        <h1 className="gradient__text">KANDY API COTTAGE</h1>
 
-      <p>
-        Escape to the Cottage with you and your loved ones and enjoy the place
-        like it’s your own house.
-      </p>
+        <p>
+          Escape to the Cottage with you and your loved ones and enjoy the place
+          like it’s your own house.
+        </p>
 
-      {/* <div className='header-content__people'>
+        {/* <div className='header-content__people'>
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div> */}
+      </div>
+      <div className="header-image" data-aos="fade-left">
+        <img src={header} alt="header" />
+      </div>
     </div>
-    <div className="header-image">
-      <img src={header} alt="header" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/containers/intro/Intro.jsx
+++ src/containers/intro/Intro.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Feature from "../../components/feature/Feature";
 import "./intro.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import bath from "../../assets/bath.svg";
 import sofa from "../../assets/sofa.svg";
 import bed from "../../assets/bed.svg";
 
 const Intro = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
   return (
-    <div className="intro section__margin" id="wgpt3">
+    <div className="intro section__margin" id="about">
       <div className="intro-heading">
-        <h1 className="gradient__text">
+        <h1
+          className="gradient__text"
+          data-aos="fade-right"
+          data-aos-offset="500"
+        >
           A Cottage Where Modern Meets Tradition in an Ancident City
         </h1>
-        <a href="#gallery">
+        <a href="#gallery" data-aos="fade-left" data-aos-offset="500">
           <p>Explore our Gallery</p>
         </a>
       </div>
 
-      <div className="intro-container">
+      <div className="intro-container" data-aos="fade-up" data-aos-offset="500">
         <Feature
           title="Living Room"
           text="Airconditioned, Satalite TV and Free WiFi avaiable"
